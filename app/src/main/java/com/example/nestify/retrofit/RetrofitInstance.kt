d866263f1ac9091321b23edb20ba6197f3b52635
rookiecoder910 <carnageinside192@gmail.com>
2025-11-12 23:46:47 +0530

    ++1

---
+++ app/src/main/java/com/example/nestify/retrofit/RetrofitInstance.kt
@@ -0,0 +1,6 @@
+package com.example.nestify.repository.retrofit
+// use retofit to call external apis such as google maps
+object RetrofitInstance {
+
+
+}
---
+++ app/src/main/java/com/example/nestify/retrofit/RetrofitInterface.kt
@@ -0,0 +1,4 @@
+package com.example.nestify.repository.retrofit
+//create an instance  of retrofit
+interface RetrofitInterface {
+}
